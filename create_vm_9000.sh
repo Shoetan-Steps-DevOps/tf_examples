09080201ebfff41fa0ed349e5502372a9362c819
ashoetan <alfredshoetan@gmail.com>
2023-08-03 20:08:26 -0400

    Update create_vm_9000.sh

--- create_vm_9000.sh
+++ create_vm_9000.sh
@@ -16,24 +16,22 @@ fi
 #    exit
 # fi
 
-# FILE2=/root/jammy-server-cloudimg-amd64.img.original
-FILE2=/root/lunar-server-cloudimg-amd64.img.original
-
+FILE2=/root/jammy-server-cloudimg-amd64.img.original
 if test -f "$FILE2"; then
      echo "found img file skipping download..."
-     cp /root/lunar-server-cloudimg-amd64.img.original /root/lunar-server-cloudimg-amd64.img
+     cp /root/jammy-server-cloudimg-amd64.img.original /root/jammy-server-cloudimg-amd64.img
 else
      echo "downloading img file..."
      cd /root/
-     wget https://cloud-images.ubuntu.com/jammy/current/lunar-server-cloudimg-amd64.img
+     wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 fi
 
-virt-customize -a lunar-server-cloudimg-amd64.img --install qemu-guest-agent
-virt-customize -a lunar-server-cloudimg-amd64.img --run-command "useradd -m -s /bin/bash ansible"
-virt-customize -a lunar-server-cloudimg-amd64.img --root-password password:ubuntu
-# virt-customize -a lunar-server-cloudimg-amd64.img --ssh-inject ubuntu:file:/root/ansible_ssh_key.txt
+virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
+virt-customize -a jammy-server-cloudimg-amd64.img --run-command "useradd -m -s /bin/bash ubuntu"
+virt-customize -a jammy-server-cloudimg-amd64.img --root-password password:ubuntu
+# virt-customize -a jammy-server-cloudimg-amd64.img --ssh-inject ubuntu:file:/root/ansible_ssh_key.txt
 qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
-qm set 9000 --scsi0 local-lvm:0,import-from=/root/lunar-server-cloudimg-amd64.img
+qm set 9000 --scsi0 local-lvm:0,import-from=/root/jammy-server-cloudimg-amd64.img
 qm set 9000 --ide2 local-lvm:cloudinit
 qm set 9000 --boot order=scsi0
 qm set 9000 --serial0 socket --vga serial0
